d1ae8b2dca6fa2d5c48ee4ce3e0bde04ac1c81a7
GULLAPALLIBABJICHANDRASEKHAR <hemalathasamudrala20@gmail.com>
2025-02-24 21:01:10 +0530

    Lockscreen password set

--- js/app.js
+++ js/app.js
@@ -8,6 +8,54 @@ function unlock() {
 }
 unlock();
 
+// unlocking window
+function unlockScreen() {
+    const container = document.querySelector(".lockscreen-password-container");
+    const passKey = document.getElementById("passKey");
+    const unlockBtn = document.getElementById("unlockBtn");
+    const question = document.getElementById("question");
+    const tryAgainMsg = document.getElementById("tryAgainMsg");
+    const randNum1 = Math.floor(Math.random() * 10);
+    const randNum2 = Math.floor(Math.random() * 10);
+    console.log(randNum1, randNum2);
+    const result = randNum1 + randNum2;
+    console.log(result)
+    question.innerText = randNum1 + " + " +randNum2;
+
+    unlockBtn.addEventListener("click", ()=> {
+        if (parseInt(passKey.value) == result) {
+            container.classList.add("unlockscreen");
+        }
+        else {
+            tryAgainMsg.innerText = "Please try again";
+        }
+    });
+}
+unlockScreen();
+
+// Windows Container Appear/Disappear
+function windowsBtnAppear() {
+    const windowsContainer = document.querySelector(".windows-container");
+    const windowsBtn = document.querySelector(".windowsLogoBtn");
+    // Event listener to toggle the visibility of the windows container on single click
+    windowsBtn.addEventListener("click", () => {
+        // Toggle visibility of windows container
+        if (windowsContainer.classList.contains("show-windows-container")) {
+            windowsContainer.classList.remove("show-windows-container");
+        } else {
+            windowsContainer.classList.add("show-windows-container");
+        }
+    });
+    // Event listener to close the container if clicked outside of it
+    document.addEventListener("click", (e) => {
+        if (!windowsContainer.contains(e.target) && !windowsBtn.contains(e.target)) {
+            windowsContainer.classList.remove("show-windows-container");
+        }
+    });
+}
+windowsBtnAppear();
+// Call the function to enable the functionality
+
 // theme change from dark to light & light to dark
 function changeTheme() {
     const themeBtn = document.querySelector(".themeBtn");
